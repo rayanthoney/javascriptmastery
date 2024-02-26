9ad5fb83b9075cd82eba69c9f02c2efa9b02d63e
Ray Anthoney <rmaxey911@gmail.com>
2024-02-26 16:45:14 -0700

    setting up the home page

---
+++ jsm_react-dashboard-refine-2024/big-toes-run/src/components/home/upcoming-events.tsx
@@ -0,0 +1,28 @@
+import { CalendarOutlined } from "@ant-design/icons"
+import { Card } from "antd"
+import { Text } from "../text"
+
+const UpcomingEvents = () => {
+  return (
+    <Card 
+      style={{ height: '100%' }} 
+      headStyle={{padding: '8px 16px'}} 
+      bodyStyle={{padding: '0 1rem'}}
+      title={
+        <div style={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: '8px'
+        }}>
+          <CalendarOutlined />
+          <Text>
+            Upcoming Events
+          </Text>
+        </div>
+      }
+      >      
+    </Card>
+  )
+}
+
+export default UpcomingEvents
