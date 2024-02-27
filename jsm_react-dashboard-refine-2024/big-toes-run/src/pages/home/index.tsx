96da0d2f7e097b74704aca7cc80ce665a324e510
Ray Anthoney <rmaxey911@gmail.com>
2024-02-27 05:19:36 -0700

    home imports

--- jsm_react-dashboard-refine-2024/big-toes-run/src/pages/home/index.tsx
+++ jsm_react-dashboard-refine-2024/big-toes-run/src/pages/home/index.tsx
@@ -1,12 +1,36 @@
-import { Col, Row } from "antd"
+import { DealsChart, UpcomingEvents } from "@/components";
+import { Col, Row } from "antd";
 
 export const Home = () => {
   return (
     <div>
-      <Row>
-        <Col>
+      <Row
+        gutter={[32, 32]}
+        style={{
+          marginTop: '32px'
+        }}
+      >
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: '460px'
+          }}
+        >
+          <UpcomingEvents />
+        </Col>
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: '460px'
+          }}
+        >
+          <DealsChart />
         </Col>
       </Row>
     </div>
-  )
-}
+  );
+};
