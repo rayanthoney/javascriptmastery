a26ee1bc16b9ca7a7ff6e494ba490d6714cb79c8
Ray Anthoney <rmaxey911@gmail.com>
2024-02-28 21:00:41 -0700

    prepared the upcoming-events

--- jsm_react-dashboard-refine-2024/big-toes-run/src/components/home/upcoming-events.tsx
+++ jsm_react-dashboard-refine-2024/big-toes-run/src/components/home/upcoming-events.tsx
@@ -1,8 +1,21 @@
 import { CalendarOutlined } from "@ant-design/icons"
-import { Card } from "antd"
+import { Badge, Card, List } from "antd"
 import { Text } from "../text"
+import { useState } from "react"
+import UpcomingEventsSkeleton from "../skeleton/upcoming-events"
+import { getDate } from "@/utilities/helpers"
+import { useList } from "@refinedev/core"
+import { DASHBORAD_CALENDAR_UPCOMING_EVENTS_QUERY } from "@/graphql/queries"
 
 const UpcomingEvents = () => {
+  const [isLoading, setIsLoading] = useState(false)
+
+  const { data, isLoading: eventsLoading } = useList({
+    resource: "events",
+    meta: {
+      gqlQuery: DASHBORAD_CALENDAR_UPCOMING_EVENTS_QUERY
+    }
+  });
   return (
     <Card 
       style={{ height: '100%' }} 
@@ -20,7 +33,36 @@ const UpcomingEvents = () => {
           </Text>
         </div>
       }
-      >      
+      >
+        {isLoading ? (
+          <List
+            itemLayout="horizontal"
+            dataSource={Array.from({ length: 5 }).map((_, index) => ({
+              id: index, 
+            }))}
+            renderItem={() => <UpcomingEventsSkeleton />}
+          />
+        ) : (
+          <List
+            itemLayout="horizontal"
+            dataSource={[]}
+            renderItem={(item) => {
+                const renderDate = getDate(item.startDate, item.endDate)
+
+              return (
+                <List.Item>
+                  <List.Item.Meta 
+                    avatar={<Badge color={item.color} />}
+                    title={<Text size="xs">{renderDate}</Text>}
+                    description={<Text ellipsis={{ tooltip: true}} strong>{item.title}</Text>}
+                  />
+                </List.Item>
+              )
+            }}
+          >
+
+          </List>
+        )}
     </Card>
   )
 }
